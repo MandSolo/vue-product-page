06d5e279fee0d9da30e996fb49ca63b4adb06546
Mand Cashin <mandcashin@hotmail.co.uk>
2020-03-25 19:48:41 +0000

    refactor: update product method for selectedVariant

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var app = new Vue({
   data: {
     brand: "Vue Mastery",
     product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
+    selectedVariant: 0,
     inStock: false,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
@@ -24,13 +24,16 @@ var app = new Vue({
     addToCart() {
       this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
     }
   },
   computed: {
     title() {
       return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
     }
   }
 });
